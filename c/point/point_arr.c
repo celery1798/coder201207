7137f75e06ee1d5c7e64298bee66d188d63bbcb4
celery1798 <celery1798@163.com>
2020-12-22 15:07:09 +0800

    new__mod_

---
+++ c/point/point_arr.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+arr[i] => *(arr+i) => *(p+i) => p[i]
+
+&arr[i] => arr+i => p+i => &p[i]
+
+2 + 3  = 3+2
+*(p+i) = *(i+p) => i[p]
+
+*/
+
+int main()
+{
+//	p++  p+1
+	int arr[5],i;
+	int *p = arr;
+
+	for(i = 0 ; i < sizeof(arr)/sizeof(arr[0]); i++,p++)
+		scanf("%d",p);
+
+	p = arr;
+	for(i = 0 ; i < sizeof(arr)/sizeof(arr[0]); i++,p++)
+		printf("%d ",*p);
+	printf("\n");	
+
+
+
+
+
+/*	
+	int arr[] = {225,64,67,28,9};
+	int *p = &arr[0] , *q = &arr[3];
+
+
+	printf("%ld\n",q - p);
+	printf("%d\n",*q - *p);
+
+*/
+
+
+
+/*
+	int arr[] = {5,6,7,8,9};
+	int *p = arr;	// &arr[0];
+	int i;
+
+	for(i = 0 ; i < sizeof(arr)/sizeof(arr[0]); i++)
+		//printf("%p --> %d\n",arr+i,*(arr+i));
+		printf("%p --> %d\n",&p[i],p[i]);
+		//printf("%p --> %d\n",p+i,*(p+i));
+		//printf("%p --> %d\n",&arr[i],arr[i]);
+	printf("\n");
+*/
+	
+	exit(0); 	//return 0;
+}
+
