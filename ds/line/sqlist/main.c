01b24ffb6a0bce421eb4e20a3b3f95dc9d06f509
celery1798 <celery1798@163.com>
2020-12-29 10:41:17 +0800

    new__mod_

--- ds/line/sqlist/main.c
+++ ds/line/sqlist/main.c
@@ -6,8 +6,9 @@
 
 int main()
 {
-	SQLIST *sq;
-	data_t a[] = {4,5,6,7,8,9};
+	SQLIST *sq,*sq1;
+	data_t a[] = {14,5,26,7,38,9};
+	data_t b[] = {14,25,26,47,38,99};
 	int i;
 
 	sq = sqlist_init();
@@ -16,15 +17,46 @@ int main()
 		printf("sqlist_init() error!\n");
 		exit(1);	
 	}
-		
+
 	for(i = 0 ; i < sizeof(a)/sizeof(*a); i++)	
-		sqlist_insert_pos(sq,0,&a[i]);
-	
+	{
+		//	sqlist_insert_pos(sq,0,&a[i]);
+		sqlist_insert_order(sq,&a[i]);
+	}
 	sqlist_show(sq);
 
-	int value;
-	sqlist_delete_pos(sq,2,&value);
-	printf(value);
+	sq1 = sqlist_init();
+	if(sq1 == NULL)
+	{
+		printf("sqlist_init() error!\n");
+		exit(1);
+	}
+
+	for(i = 0 ; i < sizeof(b)/sizeof(*b); i++)
+	{
+		//  sqlist_insert_pos(sq,0,&a[i]);
+		sqlist_insert_order(sq1,&b[i]);
+	}
+	sqlist_show(sq1);
+
+
+	sqlist_union(sq,sq1);
+	sqlist_show(sq);
+
+
+	sqlist_destroy(sq1);
+
+
+
+
+
+/*
+	sqlist_delete_pos(sq,1,&value);
+	printf("VALUE = %d\n",value);
+	sqlist_show(sq);
+*/
+
+
 
 /*
 	int value = 100;
@@ -32,9 +64,9 @@ int main()
 	ret = sqlist_insert_pos(sq,2,&value);
 	printf("RET : %d\n",ret);
 	sqlist_show(sq);
-?
-//	sqlist_destroy(sq);
-
+*/
+	sqlist_destroy(sq);
+	
 	exit(0);
 }
 
--- ds/line/sqlist/sqlist.c
+++ ds/line/sqlist/sqlist.c
@@ -56,17 +56,121 @@ void sqlist_show(SQLIST *ptr)
 {
 	int i;
 
-	if(ptr->last != -1)
+	if(sqlist_isempty(ptr))
+		return ;
+
+	for(i = 0 ; i <= ptr->last  ; i++)
+		printf("%d ",ptr->data[i]);
+	printf("\n");
+}
+
+int sqlist_isempty(SQLIST *ptr)
+{
+	if(ptr->last == -1)
+		return 1;
+	return 0;
+}
+
+void sqlist_setempty(SQLIST *ptr)
+{
+	ptr->last = -1;
+}
+
+int sqlist_length(SQLIST *ptr)
+{
+	return ptr->last;
+}
+
+int sqlist_delete_pos(SQLIST *ptr,int pos,data_t *newdata)
+{
+	int i; 
+
+	if(sqlist_isempty(ptr))
+		return -1;
+
+	if(pos < 0 || pos > ptr->last)
+		return -2;
+
+	if(newdata != NULL)
+		*newdata = ptr->data[pos];
+
+	i = pos+1;		
+	while(i <= ptr->last)
 	{
-		for(i = 0 ; i <= ptr->last  ; i++)
-			printf("%d ",ptr->data[i]);
-		printf("\n");
+		ptr->data[i-1] = ptr->data[i];
+		i++;
 	}
-		
+	ptr->last --;
+
+	return 0;
+}
+
+int sqlist_insert_order(SQLIST *ptr,data_t *newdata)
+{
+	int pos,i;
+
+	if(sqlist_isfull(ptr))
+		return -1;
+
+	for(i = 0 ; i <= ptr->last; i++)
+	{
+		if(ptr->data[i] > *newdata)	
+		{
+			pos = i;
+			break;
+		}
+	}	
+	if(i == ptr->last+1)
+		pos = i;
+
+	return sqlist_insert_pos(ptr,pos,newdata);		
+}
+
+int sqlist_delete_value(SQLIST *ptr,data_t *newdata)
+{
+	int ret;
+
+	ret = sqlist_find(ptr,newdata);
+	if(ret >= 0)
+		return sqlist_delete_pos(ptr,ret,NULL);
+	return ret;
+
+}
+
+int sqlist_find(SQLIST *ptr,data_t *newdata)
+{
+	int i;
+
+	if(sqlist_isempty(ptr))
+		return -1;
+
+	for(i = 0 ; i <= ptr->last; i++)
+		if(ptr->data[i] == *newdata)
+			return i;
+	return -2;
 }
 
 
+void sqlist_destroy(SQLIST *ptr)
+{
+	free(ptr);
+//	ptr = NULL;
+}
 
+//ptr1->1,3,5  ,ptr2-> 1,2,3;   ptr1->1 3 5 2
+int sqlist_union(SQLIST *ptr1,SQLIST *ptr2)
+{
+	int i;
+
+	if(sqlist_isempty(ptr2))
+		return 0;
+
+	for(i = 0 ; i <= ptr2->last	; i++)
+		if(sqlist_find(ptr1, &ptr2->data[i]) == -2)
+			sqlist_insert_pos(ptr1,ptr1->last+1,&ptr2->data[i]);
+	
+	return 0;
+}
 
 
 
