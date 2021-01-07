43029238644320a95bdebb5b321e873c5009cc96
celery1798 <celery1798@163.com>
2021-01-07 11:58:49 +0800

    new__mod_

---
+++ ds/tree/bitree/bitree.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NAMESIZE    32
+
+struct score
+{
+	int id;
+	char name[NAMESIZE];
+	int math;
+};
+
+typedef struct node_st
+{
+	struct score data;
+	struct node_st *l,*r;
+}TREE;
+
+void print_s(void *data)
+{
+	struct score *d = data;
+	printf("%d %s %d\n",d->id,d->name,d->math);
+}
+
+int insert(TREE **root,struct score *data)
+{
+	struct node_st *node;
+
+	if(*root == NULL)
+	{
+		node = malloc(sizeof(*node));
+		if(node == NULL)
+			return -1;
+		node->data = *data;
+		node->l = node->r = NULL;
+		*root = node;
+		return 0;
+	}
+
+	if(data->id <= (*root)->data.id)
+		return insert(&(*root)->l,data);
+	return insert(&(*root)->r,data);
+}
+
+int main()
+{
+	struct score tmp;
+	TREE *tree = NULL;
+	int arr[] = {1,2,3,7,6,5,9,8,4};
+	int i;
+
+	for(i = 0 ;i < sizeof(arr)/sizeof(*arr); i++)
+	{
+		tmp.id = arr[i];
+		snprintf(tmp.name,NAMESIZE,"Stu%d",arr[i]);
+		tmp.math = 100-arr[i];
+
+		insert(&tree,&tmp);
+	}
+
+
+	
+//	draw();
+
+
+	exit(0);
+}
+
+
+
+
+
+
+
+
+
+
+
