66b1e7014cc02945e81275c02a50a315e0982e37
celery1798 <celery1798@163.com>
2021-02-23 15:59:43 +0800

    new__mod_

--- apue/ipc/socket/dgram/basic/snder.c
+++ apue/ipc/socket/dgram/basic/snder.c
@@ -1,21 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 
 #include "proto.h"
 
-int main()
+int main(int argc,char **argv)
 {
+	struct sockaddr_in raddr;
+	struct msg_st sbuf;
+	int sd;
 
-	socket();
+	if(argc < 2)
+	{
+		fprintf(stderr,"Usage...\n");
+		exit(1);	
+	}
+
+	sd = socket(AF_INET,SOCK_DGRAM,0/*IPPROTO_UDP*/);
+	if(sd < 0)
+	{
+		perror("socket()");
+		exit(1);
+	}
+
 
 //	bind();
 
+	sbuf.id = htonl(rand()%100);
+	strcpy(sbuf.name,"Alan");
+	sbuf.math = htonl(rand()%100);
 
-	sendto();
+	raddr.sin_family = AF_INET;
+	raddr.sin_port = htons(atoi(RCVPORT));
+	inet_pton(AF_INET,argv[1],&raddr.sin_addr);
+
+	if(sendto(sd,&sbuf,sizeof(sbuf),0,(void *)&raddr,sizeof(raddr)) < 0)
+	{
+		perror("sendto");
+		exit(1);
+	}
 
 	puts("OK");
 
-	close();
+	close(sd);
 
 	exit(0);
 }
