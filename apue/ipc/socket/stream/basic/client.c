5b643f4f285d10d17ecbb748aca84b7a96735d20
celery1798 <celery1798@163.com>
2021-02-24 16:39:09 +0800

    new__mod_

---
+++ apue/ipc/socket/stream/basic/client.c
@@ -0,0 +1,14 @@
+
+
+
+
+socket
+
+connect  
+
+
+recv();
+
+
+close();
+
---
+++ apue/ipc/socket/stream/basic/proto.h
@@ -0,0 +1,15 @@
+#ifndef __PROTO_H__
+#define __PROTO_H__
+
+#define SERVERPORT		"1989"
+
+
+#define FMT_STAMP		"%lld"
+
+
+#endif
+
+
+
+
+
